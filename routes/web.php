64c4ad560cb9d7dacc722d34a99460e1ec2658d6
giovanyrosales <gioer21@gmail.com>
2022-04-20 15:18:16 -0600

    arregle ruta perfil

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\Login\LoginController;
 use App\Http\Controllers\Controles\ControlController;
-use App\Http\Controllers\Backend\Perfil\PerfilControl;
+use App\Http\Controllers\Backend\Perfil\PerfilController;
 use App\Http\Controllers\Backend\RolesPermisos\RolesController;
 use App\Http\Controllers\Backend\RolesPermisos\PermisosController;
 use App\Http\Controllers\Backend\Proveedor\ProveedorController;
